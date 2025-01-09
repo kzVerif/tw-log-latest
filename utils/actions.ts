3f956f2f352ad26065d5927b94a88c0997e97917
kzVerif <165546547+kzVerif@users.noreply.github.com>
2025-01-09 23:32:26 +0700

    Fetch to mdgp url

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,5 @@
 export async function getAll() {
-    const res = await fetch('/api',{
+    const res = await fetch('https://www.mdgp-backend.store/api',{
         method: 'GET'
     })
     const data = await res.json()
