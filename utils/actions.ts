6814155a834df6ad6b7578456da74b0e8be1257a
kzVerif <165546547+kzVerif@users.noreply.github.com>
2025-01-10 00:28:24 +0700

    fix format time

--- utils/actions.ts
+++ utils/actions.ts
@@ -7,24 +7,29 @@ export async function getAll() {
 }
 
 export function formatDateTime(isoString: string) {
-    const date = new Date(isoString);
-  
-    const day = date.getUTCDate().toString().padStart(2, '0'); // วันที่
-    const monthNames = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน",
-      "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"
-    ];
-    const month = monthNames[date.getUTCMonth()]; // เดือน
-    const year = (date.getUTCFullYear() + 543).toString(); // ปี (พ.ศ.)
-    let hours = date.getUTCHours().toString().padStart(2, '0'); // ชั่วโมง
-    let minutes = date.getUTCMinutes().toString().padStart(2, '0'); // นาที
-  
-     // ปรับเวลาให้เป็น 00:00 ถ้าเป็นวันใหม่
-      if(hours == "24"){
-          hours = "00"
-      }
-      if(minutes == "60"){
-          minutes = "00"
-      }
-  
-    return `${day}-${month}-${year} ${hours}:${minutes}`;
-  }
+    try {
+        const date = new Date(isoString);
+
+        const day = date.getUTCDate().toString().padStart(2, '0');
+        const monthNames = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน",
+            "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"
+        ];
+        const month = monthNames[date.getUTCMonth()];
+        const year = (date.getUTCFullYear() + 543).toString();
+        let hours = date.getUTCHours().toString().padStart(2, '0');
+        let minutes = date.getUTCMinutes().toString().padStart(2, '0');
+
+        // ปรับเวลาให้เป็น 00:00 ถ้าเป็นวันใหม่
+        if (hours == "24") {
+            hours = "00";
+        }
+        if (minutes == "60") {
+            minutes = "00";
+        }
+
+        return `${day}-${month}-${year} ${hours}:${minutes}`;
+    } catch (error) {
+        console.error("Error formatting date:", error);
+        return "Invalid Date"; // หรือจัดการ error ตามความเหมาะสม
+    }
+}
