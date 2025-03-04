6b70d90556bf1e55332b689d86b4633dd575404e
kzVerif <165546547+kzVerif@users.noreply.github.com>
2025-03-04 19:43:19 +0700

    ADD SNAPZ STORE API WEBHOOK

---
+++ app/api/snapz/route.js
@@ -0,0 +1,72 @@
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import { revalidatePath } from "next/cache";
+
+let trans = [];
+
+export async function GET() {
+  return NextResponse.json({ Messaage: "Bello" });
+}
+
+
+export async function POST(req) {
+  const data = await req.json();
+  const decode = jwt.decode(data.message);
+
+  if (!decode) {
+    return NextResponse.json({ error: "Invalid JWT payload" }, { status: 400 });
+  }
+
+  let fee = ((decode.amount / 100)*0.029)
+    
+  if (fee >= 10) {
+    fee = 10
+  }
+
+  const send = {
+    username: "Snapz-BOT",
+    embeds: [
+        {
+            avatar_url: "https://cdn.discordapp.com/attachments/1183055670677864559/1333707287646441593/16c9118e7f0d4766.jpg?ex=6799df34&is=67988db4&hm=ae6f0a998bca06dcef3eb84d300ff2cb773469fa478a81f015d0a61d85464660&",
+            title: "📢 **แจ้งเตือนการรับเงิน True Wallet**",
+            color: 0x00ff00,  // สีเขียว
+            fields: [
+                {
+                    name: "__ช่องทางการรับ__",
+                    value: `*${decode.event_type}*`,
+                    inline: true
+                },
+                {
+                    name: "__จำนวนเงิน__",
+                    value: `**฿${((decode.amount / 100)).toLocaleString()}**`,
+                    inline: false
+                },
+                {
+                    name: "__เบอร์ผู้โอน__",
+                    value: `**${decode.sender_mobile || 'ไม่ระบุ'}**`,
+                    inline: false
+                },
+                {
+                    name: "⏱ เวลาที่ได้รับ",
+                    value: `<t:${Math.floor(new Date(decode.received_time).getTime() / 1000)}:F>`,
+                }
+            ],
+            footer: {
+                text: "Powered by KzVrf.",
+                icon_url: "https://i.imgur.com/fKL31aD.jpg"
+            }
+        }
+    ]
+  };
+
+  await fetch(
+    "https://discord.com/api/webhooks/1333696110283198464/VUMcfFmBfDZJCWg_jJOllZ6kJs5d-qulTqzSqOKOtEAddnPvONxj3-GP5MlJHNhEC8M_",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(send),
+    }
+  );
+
+  return NextResponse.json({ decode });
+}
